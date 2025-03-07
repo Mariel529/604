0aade15a19542d4e39b66a56d3d9ec1f0b9e23ee
Mariel529 <1991826203@qq.com>
2025-03-07 10:59:36 +0800

    1.py

--- 604 week3/1.py
+++ 604 week3/1.py
@@ -1,6 +1,6 @@
-import socket
+iimport socket
 import platform
-
+# Define a function to get the machine's IP address
 def get_machine_ip():
     # Get the hostname
     hostname = socket.gethostname()
@@ -12,10 +12,13 @@ def get_machine_name():
     # Get the machine name using platform
     machine_name = platform.node()
     return machine_name
-
+# Main execution block
 if __name__ == "__main__":
+    #Get the machine's IP address using the defined function
     ip = get_machine_ip()
+    #Get the machine's name using the defined function
     name = get_machine_name()
+    # Load: Print the extracted information along with a custom message
     print(f"Machine IP: {ip}")
     print(f"Machine Name: {name}")
     print("my name is Mariel")
